e5d9267c151c15de5b3b5f7df3e7069b8a487904
NSpahic22 <nspahi22@student.aau.dk>
2023-03-20 16:26:19 +0100

    Hjemme side 2.0
    Den er meget bedre nu med features som f.eks.:
    Et kort
    Indtastning af adresser

---
+++ main.js
@@ -0,0 +1,29 @@
+function initMap () {
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    const directionsService = new google.maps.DirectionsService();
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 14,
+        center: {lat:57.04, lng: 9.93},
+    });
+     
+    directionsRenderer.setMap(map);
+    caluclateAndDisplayRoutes(directionsService, directionsRenderer);
+    document.getElementById("mode").addEventListener("change", () => {
+        caluclateAndDisplayRoutes(directionsService, directionsRenderer);
+    });
+}
+
+function caluclateAndDisplayRoutes(directionsService, directionsRenderer) {
+    const selectedMode = document.getElementById("mode").value;
+    directionsService
+    .route({
+        origin: document.getElementById("from").value,
+        destination: document.getElementById("to").value,
+
+        travelMode: google.maps.TravelMode[selectedMode],
+    })
+    .then((response) => {
+        directionsRenderer.setDirections(response);
+    })
+    .catch((e) => window.alert("Directions request failed due to " + status));
+}
