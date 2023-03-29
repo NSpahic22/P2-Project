ef32a63ec19bd07bc58eb058a44a17bccedb80bf
NSpahic22 <nspahi22@student.aau.dk>
2023-03-29 13:19:40 +0200

    From div to forms and other

--- main.js
+++ main.js
@@ -1,33 +1,44 @@
+//This is function is called with our API key
 function initMap () {
+    //Assigning google functions
     const directionsRenderer = new google.maps.DirectionsRenderer({
         draggable: true,
     });
     const directionsService = new google.maps.DirectionsService();
+
+    //Creates maps
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 14,
         center: {lat:57.04, lng: 9.93},
     });
-     
+    
+    //Sets map onto our site
     directionsRenderer.setMap(map);
+    
+    //Adds eventlistener to the submit button for the addresses
     document.getElementById("mode").addEventListener("click", () => {
         caluclateAndDisplayRoutes(directionsService, directionsRenderer);
     });
-}
+}   
 
+//Function called when user clicks submit
 function caluclateAndDisplayRoutes(directionsService, directionsRenderer) {
-    const waypts = [];
-    waypts.push(document.getElementById("submitextra"+x));
-    console.log(waypts);
-    const selectedMode = document.getElementById("mode").value;
+    //The google function which:
+    //1. Fetches the route from the user, and sets the rules for creating the route
     directionsService
     .route({
         origin: document.getElementById("from").value,
         destination: document.getElementById("to").value,
-        travelMode: google.maps.TravelMode[selectedMode],
+        travelMode: 'DRIVING',
         avoidHighways: true,
     })
+    
+    //2. Creates the route
     .then((response) => {
         directionsRenderer.setDirections(response);
     })
+
+    //3. Should there be a mistakes, that makes the function unable to run,
+    //an alert will pop up on the website
     .catch((e) => window.alert("Directions request failed due to failed input"));
 }
--- route_diversions.js
+++ route_diversions.js
@@ -1,20 +1,46 @@
+//Creates event listener for when user clicks the + button
 document.querySelector('input[value="+"]').addEventListener("click", appendinator);
+document.querySelector('input[name="Submit"]').addEventListener("click", morestops);
 
-const test = document.querySelector('input[value="+"]');
-console.log(test);
+//Helps create unique ids for each address
+let inputcounter = 1;
+let addcounter = 0;
 
+//The action of the plus button
 function appendinator(event) {
+    //Creates new input bar and linebreak
     const inputcreater = document.createElement("input");
     const linebreaker = document.createElement("br");
-    inputcreater.placeholder = "Input extra destination";
 
+    //Gives input bar text and an ID
+    inputcreater.placeholder = "Input extra destination";
+    inputcreater.setAttribute("id", 'address' + inputcounter++)
+ 
+    //Appends the input bar and linebreaker to the HTML-code
     const extraplaces = document.getElementById("extraplaces");
     extraplaces.appendChild(inputcreater);
     extraplaces.appendChild(linebreaker);
    }
 
-function morestops(){
-    const sted = document.getElementById("extrastop").value;
+function morestops(event, addressarr){
+    const waypointarr = [];
+    let i = 0;
+
+    for (i; i < 100; i++) {
+        const checker = document.getElementById("address"+i).value;
+        
+        if (checker != null) {
+            waypointarr[a] = checker;
+            a++;
+        }
+        
+        else if(checker === null) {
+            console.log("error");
+        }
+    }
     
-    google.maps.Marker
+
+
+
+    
 }
