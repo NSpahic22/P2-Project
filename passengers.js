184a6d08438b83420b05694dd0a8d43fb1701823
JensG <do38vu@student.aau.dk>
2023-04-11 14:20:26 +0200

    css + average passengers v0.5
    :D

---
+++ passengers.js
@@ -0,0 +1,7 @@
+document.querySelector('input[name="passengersubmit"]').addEventListener("click", passengersinput);
+
+function passengersinput(){
+    weeklyavg = document.querySelector('input[name="weeklypassengers"]').value
+    outputplace = document.getElementById("")
+    
+}
