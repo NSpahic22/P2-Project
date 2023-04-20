44260b83a9829d74888b856ac420b3169fd5cadf
sourtendon <113062702+sourtendon@users.noreply.github.com>
2023-04-20 03:06:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,73 +1,63 @@
-let busStopMarkers = [];
+let busStopMarker;
 let map;
-let waypointarr;
 
-
-//This is function is called with our API key
-function initMap () {
-    //Assigning google functions
-    const directionsRenderer = new google.maps.DirectionsRenderer({
-        //draggable: true,
-    });
-    const directionsService = new google.maps.DirectionsService();
-    
-     //Creates maps
-    map = new google.maps.Map(document.getElementById("map"), {
+function initMap() {
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  const directionsService = new google.maps.DirectionsService();
+  map = new google.maps.Map(document.getElementById("map"), {
     zoom: 14,
-    center: {lat:57.04, lng: 9.93},
-    });
+    center: { lat: 57.04, lng: 9.93 },
+  });
+  directionsRenderer.setMap(map);
+  document.getElementById("mode").addEventListener("click", () => {
+    calculateAndDisplayRoute(directionsService, directionsRenderer);
+  });
+}
 
-    //Sets map onto our site
-    directionsRenderer.setMap(map);
-    
-    //Adds eventlistener to the submit button for the addresses
-    document.getElementById("mode").addEventListener("click", () => {
-        caluclateAndDisplayRoutes(directionsService, directionsRenderer);
-    });
-}   
-
-
-//Function called when user clicks submit
-function caluclateAndDisplayRoutes(directionsService, directionsRenderer) {
-    // waypoint array
-    const waypointarr = [];
-
-    //The google function which:
-    //1. Fetches the route from the user, and sets the rules for creating the route
-    directionsService.route({
-        origin: document.getElementById("from").value,
-        destination: document.getElementById("to").value,
-        travelMode: 'DRIVING',
-        avoidHighways: true,
-        waypoints: waypointarr,
-        optimizeWaypoints: true,
-        })
-    
-    
-    //2. Creates the route
-      .then((response) => {
-        directionsRenderer.setDirections(response);
-        
-        let routeStart = response.routes[0].legs[0].start_location; 
-        let route = response.routes[0].overview_path;
-        let distanceTraveled = 1000;
-        let markerPosition = google.maps.geometry.spherical.computeOffset(routeStart, distanceTraveled, google.maps.geometry.spherical.computeHeading(routeStart, route[1]));
-
-        let busStop = new google.maps.Marker({
-          position: markerPosition,
-          map: map,
-          title: "first bus stop"
-        });
-
-        const distanceToMarker = google.maps.geometry.spherical.computeDistanceBetween(startLocation, markerPosition);
-        const closestPolylinePoint = google.maps.geometry.poly.isLocationOnEdge(markerPosition, path, 10e-2) ? markerPosition : path[google.maps.geometry.poly.computeDistanceBetween(markerPosition, path[0]) > google.maps.geometry.poly.computeDistanceBetween(markerPosition, path[path.length-1]) ? path[path.length-1] : path[0]];
-        marker.setPosition(closestPolylinePoint);
-
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  const waypoints = [];
+  directionsService
+    .route({
+      origin: document.getElementById("from").value,
+      destination: document.getElementById("to").value,
+      travelMode: "DRIVING",
+      avoidHighways: true,
+      waypoints: waypoints,
+      optimizeWaypoints: true,
     })
-    
-      //3. Should there be a mistakes, that makes the function unable to run,
-      //an alert will pop up on the website
-      if (status !== "OK") {
-        (e) => window.alert("Directions request failed due to failed input" + status);
-      }
-    };
+    .then((response) => {
+      directionsRenderer.setDirections(response);
+      let route = response.routes[0].overview_path;
+      let routeStart = response.routes[0].legs[0].start_location;
+      let distance = 1000;
+      let markerPosition = google.maps.geometry.spherical.computeOffset(
+        routeStart,
+        distance,
+        google.maps.geometry.spherical.computeHeading(routeStart, route[1])
+      );
+      busStopMarker = new google.maps.Marker({
+        position: markerPosition,
+        map: map,
+        title: "Bus stop",
+      });
+      let polyline = new google.maps.Polyline({
+        path: route,
+        strokeColor: "#FF0000",
+      });
+      let closestPolylinePoint = google.maps.geometry.poly.isLocationOnEdge(
+        busStopMarker.getPosition(),
+        polyline,
+        10e-2
+      )
+        ? busStopMarker.getPosition()
+        : google.maps.geometry.spherical.interpolate(
+            routeStart,
+            busStopMarker.getPosition(),
+            0.5
+          );
+      busStopMarker.setPosition(closestPolylinePoint);
+    })
+    .catch((e) => {
+      window.alert("Directions request failed due to " + status);
+    });
+}
