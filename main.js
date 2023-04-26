12701def38acc11a0fb8f05c96ccdf9684b86b95
sourtendon <113062702+sourtendon@users.noreply.github.com>
2023-04-26 12:52:45 +0200

    lol

--- main.js
+++ main.js
@@ -1,91 +1,90 @@
-let busStopMarker;
-let map;
-let startLocation = null;
-let endLocation = null;
-let waypts = [];
-let markersArray = [];
 let distance = 1000;
 
-const randomCords = [{ lat: 57.04, lng: 9.93 }, { lat: 58, lng: 10 }];
+            google.maps.LatLng.prototype.distanceFrom = function(newLatLng) {
+                var EarthRadiusMeters = 6378137.0;
+                var lat1 = this.lat();
+                var lon1 = this.lng();
+                var lat2 = newLatLng.lat();
+                var lon2 = newLatLng.lng();
+                var dLat = (lat2-lat1) * Math.PI / 180;
+                var dLon = (lon2-lon1) * Math.PI / 180;
+                var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                    Math.cos(lat1 * Math.PI / 180 ) * Math.cos(lat2 * Math.PI / 180 ) *
+                    Math.sin(dLon/2) * Math.sin(dLon/2);
+                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+                var d = EarthRadiusMeters * c;
+                return d;
+            }
 
-function GetPointAtDistance(metres) {
-  if (metres === 0) return this.getPath().getAt(0);
-  if (metres < 0) return null;
-  if (this.getPath().getLength() < 2) return null;
-  let dist=0;
-  let olddist=0;
-  for (let i=1; (i < this.getPath().getLength() && dist < metres); i++) {
-    olddist = dist;
-    dist += this.getPath().getAt(i).distanceFrom(this.getPath().getAt(i-1));
-  }
-  if (dist < metres) {
-    return null;
-  }
-  let p1= this.getPath().getAt(i-2);
-  let p2= this.getPath().getAt(i-1);
-  let m = (metres-olddist)/(dist-olddist);
-  return new google.maps.LatLng( p1.lat() + (p2.lat()-p1.lat())*m, p1.lng() + (p2.lng()-p1.lng())*m);
-    //return google.maps.geometry.spherical.interpolate(p1,p2,m)
-}
+            google.maps.Polyline.prototype.GetPointAtDistance = function(metres) {// Stolen from http://www.geocodezip.com/scripts/v3_epoly.js
+                if (metres == 0) return this.getPath().getAt(0);
+                if (metres < 0) return null;
+                if (this.getPath().getLength() < 2) return null;
+                var dist=0;
+                var olddist=0;
+                for (var i=1; (i < this.getPath().getLength() && dist < metres); i++) {
+                    olddist = dist;
+                    dist += this.getPath().getAt(i).distanceFrom(this.getPath().getAt(i-1));
+                }
+                if (dist < metres) {
+                    return null;
+                }
+                var p1= this.getPath().getAt(i-2);
+                var p2= this.getPath().getAt(i-1);
+                var m = (metres-olddist)/(dist-olddist);
+                return new google.maps.LatLng( p1.lat() + (p2.lat()-p1.lat())*m, p1.lng() + (p2.lng()-p1.lng())*m);
+            }
 
+            function createMarker(map, latlng) {
+                let marker = new google.maps.Marker({
+                position: latlng,
+                 map: map,
+                });
+                markersArray.push(marker)
+            }
+            
 
-function createMarker(map, latlng) {
-    let marker = new google.maps.Marker({
-      position: latlng,
-      map: map,
-    });
-    markersArray.push(marker)
-}
+            const map = new google.maps.Map(document.getElementById("map"),{
+                zoom: 14,
+                center: { lat: 57.04, lng: 9.93 }
+            });
 
+            const directionsRenderer = new google.maps.DirectionsRenderer({preserveViewport: true});
 
-function initMap() {
-  const directionsRenderer = new google.maps.DirectionsRenderer();
-  const directionsService = new google.maps.DirectionsService();
-  map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 14,
-    center: { lat: 57.04, lng: 9.93 },
-  });
-  directionsRenderer.setMap(map);
-  document.getElementById("mode").addEventListener("click", () => {
-    calculateAndDisplayRoute(directionsService, directionsRenderer);
-  });
-}
+            directionsRenderer.setMap(map);
 
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-  //The google function which:
-  //1. Fetches the route from the user, and sets the rules for creating the route
-  directionsService
-  .route({
-      origin: document.getElementById("from").value,
-      destination: document.getElementById("to").value,
-      travelMode: 'DRIVING',
-      avoidHighways: true,
-      waypoints: waypointarr,
-      optimizeWaypoints: true,
-  })
-  
-  
-  //2. Creates the route
-  .then((response) => {
-    directionsRenderer.setDirections(response);
-      
-      const polylineRoute = new google.maps.Polyline({
-        path: response.routes[0].overview_path,
-        geodesic: true,
-        strokeColor: "#FF0000",
-        strokeOpacity: 1.0,
-        strokeWeight: 2,
-      });
+            const directions = new google.maps.DirectionsService();
 
-      polylineRoute.setMap(map);
-    
-      createMarker(map, polylineRoute.GetPointAtDistance(1000));
-      
-  })
+            directions.route({
+                origin: "stolpedalsvej 61A",
+                destination: "kærby",
+                travelMode: "DRIVING",
+                avoidHighways: true,
+                waypoints: waypointarr,
+                optimizeWaypoints: true,
+            },(data) => {
+                const polyline = new google.maps.Polyline({
+                  strokeColor: "#FF0000",
+                });
 
-  //3. Should there be a mistakes, that makes the function unable to run,
-  //an alert will pop up on the website
-  .catch((e) => window.alert("Directions request failed due to failed input" + status));
-}
+                var legs = data.routes[0].legs;
+                for (i=0;i<legs.length;i++) {
+                  var steps = legs[i].steps;
+                  for (j=0;j<steps.length;j++) {
+                    var nextSegment = steps[j].path;
+                    for (k=0;k<nextSegment.length;k++) {
+                      polyline.getPath().push(nextSegment[k]);
+                    }
+                  }
+                } 
 
+                polyline.setMap(map);
 
+                
+                //for (let i = 0; i*distance < /*add driving distance variable here*/; i++) {
+                  createMarker(map, polyline.GetPointAtDistance(distance));
+                //}
+                
+                map.setCenter(marker.getPosition());
+                directionsRenderer.setDirections(data);
+            });
