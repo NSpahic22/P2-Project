94cae8fdbd8c7b9462e5bf46987d8ccd7c43831d
JensG <do38vu@student.aau.dk>
2023-05-08 12:48:24 +0200

    ordentlige outputs
    :)

--- busplan.js
+++ busplan.js
@@ -7,22 +7,16 @@ let outputtext = []
 
 function passengersinput(){
     //a list of variable definitions for the different in- and outputs.
-    let routename = document.getElementById("routename");
-    
+    let routename = document.getElementById("routename").value;
     let weeklyavg = document.querySelector('input[name="weeklypassengers"]').value
     let businterval = bustime(weeklyavg);
     let busstopradius = document.querySelector('input[name="busstopradius"]').value;
-    let outputplace = document.getElementById("here!");
-    let outlist = document.createElement("div");
-    let breaker = document.createElement("br");
     let stopandtime = timecalc(totalDuration, weeklyavg, drivingdistance, busstopradius, businterval);
     let drivetime ='No route planned.'
     if(stopandtime[1]>0||stopandtime[2]>0||stopandtime[3]>0){
     drivetime = stopandtime[1] + 'h ' + stopandtime[2] + 'm ' + stopandtime[3] + 's'; }
     stops = stopandtime[0];
     businterval+=" minutes";
-    removeAllChildNodes(outputplace);
-    outlist.innerHTML="";
 
     //an array containing all the data that is printed for the user about the bus route
     outparr = [
@@ -43,24 +37,8 @@ function passengersinput(){
         "Amount of bus stops on route: ",
         "Interval between buses: "
     ];
-    outputarr[n+1]=outparr; outputarr[n]=outputtext
 
-    outlist.id = "additional_outputs"
-    for(let i=0; i<outparr.length; i++){
-        if(outparr[i]!=""){
-        outlist.innerHTML += outputtext[i]
-        outlist.appendChild(breaker);
-        outlist.innerHTML += outparr[i];
-        outlist.appendChild(breaker);
-
-        if(moreelements(i,outparr)==true){
-            outlist.innerHTML += "\n"
-            outlist.appendChild(breaker);
-        }
-    }
-    }
-    outputplace.append(outlist);
-
+    outputplacer(routename);
     for (let i = 0; i*busstopradius < drivingdistance*1000; i++) {
         new google.maps.Marker({
             map: map,
--- outputplacer.js
+++ outputplacer.js
@@ -1,10 +1,81 @@
-function outputplacer(outputname){
-    let submenu = document.createElement("li")
-    submenu.class = "link"
-    let dropdownmen = document.createElement("div");
-    let elementi = document.createElement("i");
-    elementi.class = "fa fa-chevron-down";
-    let items = document.createElement("ul");
-    items.class = "submenuitems"
+let submenuarr = []
+let submenunr = 0;
 
+function outputplacer(outputname) {
+    // Create the <li> element
+    const listItem = document.createElement('li');
+    listItem.className = 'link';
+  
+    // Create the <div> element for the dropdown menu
+    const dropdown = document.createElement('div');
+    dropdown.className = 'dropdown_output';
+  
+    // Create the <i> element for the dropdown icon
+    const icon = document.createElement('i');
+    icon.className = 'fa fa-chevron-down';
+  
+    // Create the text node for the dropdown title
+    const text = document.createTextNode(outputname);
+  
+    // Append the icon and text to the dropdown <div> element
+    dropdown.appendChild(icon);
+    dropdown.appendChild(text);
+  
+    // Create the <ul> element for the submenu items
+    const submenu = document.createElement('ul');
+    submenu.className = 'submenuItems';
+    submenu.id ="submenus"
+  
+    // Call the outputappender function to get the submenu content
+    const submenuContent = outputappender(outputname);
+  
+    // Append the submenu content to the submenu <ul> element
+    submenu.appendChild(submenuContent);
+  
+    submenu.addEventListener("click", function() {
+        this.classList.toggle("active");
+        let submenuItems = this.nextElementSibling;
+        if (submenuItems.style.display === "block") {
+           submenuItems.style.display = "none";
+        } else {
+           submenuItems.style.display = "block";
+        }
+     });
+
+    
+    listItem.appendChild(dropdown);
+    listItem.appendChild(submenu);
+    
+    submenuarr[submenunr] = submenu;
+
+    let parent = document.getElementById("outputsgohere")
+    let insertbefore = document.getElementById("outputbelowhere");
+    parent.insertBefore(listItem,insertbefore);
+  }
+
+  
+
+
+function outputappender(routename){
+    let outlist = document.createElement("div");
+    let breaker = document.createElement("br");
+    
+
+    outlist.id = "route:" + routename
+    outlist.innerhtml = "data for route: " + routename
+    outlist.appendChild(breaker);  
+    for(let i=0; i<outparr.length; i++){
+        if(outparr[i]!=""){
+        outlist.innerHTML += outputtext[i]
+        outlist.appendChild(breaker);
+        outlist.innerHTML += outparr[i];
+
+        if(moreelements(i,outparr)==true){
+            outlist.innerHTML += "\n"
+            outlist.appendChild(breaker);
+        }
+      }
+    }
+    console.log(outlist)
+    return outlist;
 }
