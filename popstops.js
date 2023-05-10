854c2acf8a1f1b1d227f1efca60701433977248d
JensG <do38vu@student.aau.dk>
2023-05-10 13:48:54 +0200

    o_o

--- popstops.js
+++ popstops.js
@@ -1,29 +1,17 @@
-const popstopElements = document.querySelectorAll('input[name="popstop"]');
-let popstopcheckvalue = 0;
-
-popstopElements.forEach(element => {
-    element.addEventListener('change', Popularstoppoint);
-});
 
 
-// Add a change event listener to each checkbox
-popstopElements.forEach(element => {
-  element.addEventListener('change', () => {
-    // If the current checkbox is checked, uncheck all the others
-    if (element.checked) {
-      popstopElements.forEach(otherElement => {
-        if (otherElement !== element) {
-          otherElement.checked = false;
-        }
-      });
-    }
-  });
-});
+function menuappender(){
+  let card = document.getElementByName("Startspot")[0].value;
+  let tekstfelt = document.createElement("input");
+  tekstfelt.type = "text";
+  
 
-function Popularstoppoint(event) {
-    const checkbox = event.target;
-    if (checkbox.checked) {
-      console.log(checkbox.value);
-      popstopcheckvalue = checkbox.value;
-    }
+  if (card.value == "typestart") {
+    tekstfelt.id = "from";
+    document.getElementById("Stopspot").appendChild(tekstfelt);
   }
+  if(card.value == "typestop") {
+    tekstfelt.id = "to";
+    insertbefore(tekstfelt, document.getElementById(""))
+  }
+}
