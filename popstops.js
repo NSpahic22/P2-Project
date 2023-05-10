22b9402a98cbe70857ce9265a37817f8306589d4
JensG <do38vu@student.aau.dk>
2023-05-10 14:09:26 +0200

    Popstops

--- popstops.js
+++ popstops.js
@@ -1,18 +1,21 @@
-
-
+document.getElementById("from").addEventListener("click",menuappender);
+document.getElementById("to").addEventListener("click",menuappender);
 
 function menuappender(){
-  let card = document.getElementByName("Startspot")[0].value;
+  let from = document.getElementByValue("from")[0].value;
+  let to = document.getElementByValue("to")[0].value;
   let tekstfelt = document.createElement("input");
   tekstfelt.type = "text";
   
 
-  if (card.value == "typestart") {
+  if (from.value == "typestart") {
     tekstfelt.id = "from";
+    tekstfelt.placeholder = "Starting destination"
     document.getElementById("Stopspot").appendChild(tekstfelt);
   }
   if(card.value == "typestop") {
     tekstfelt.id = "to";
-    insertbefore(tekstfelt, document.getElementById(""))
+    tekstfelt.placeholder = "Ending destination"
+    insertbefore(tekstfelt, document.getElementById("addfromabovehere"))
   }
 }
